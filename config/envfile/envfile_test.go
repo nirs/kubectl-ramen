1cb2c102fab1ab161ebe9ac1c8b9e58f430738e6
Nir Soffer <nsoffer@redhat.com>
2023-08-20 01:07:04 +0300

    Add config/envfile package
    The package provides loading of ramen environment file for adding a
    clusterset from ramen environment.

---
+++ config/envfile/envfile_test.go
@@ -0,0 +1,60 @@
+// SPDX-FileCopyrightText: The RamenDR authors
+// SPDX-License-Identifier: Apache-2.0
+
+package envfile_test
+
+import (
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"github.com/nirs/kubectl-ramen/config/envfile"
+)
+
+func TestConfigEnvfileLoadMissing(t *testing.T) {
+	missing := filepath.Join(t.TempDir(), "no-such-env.yaml")
+	_, err := envfile.Load(missing)
+	if err == nil {
+		t.Fatal("Loading missing envfile did not fail")
+	}
+}
+
+func TestConfigEnvfileLoadE2E(t *testing.T) {
+	path := filepath.Join("testdata", "e2e.yaml")
+	env, err := envfile.Load(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if env.Name != "e2e" {
+		t.Errorf("Expected name %q, got %q", "e2e", env.Name)
+	}
+
+	if env.Ramen.Hub != "hub" {
+		t.Errorf("Expected hub %q, got %q", "hub", env.Ramen.Hub)
+	}
+	clusters := []string{"dr1", "dr2"}
+	if !reflect.DeepEqual(env.Ramen.Clusters, clusters) {
+		t.Errorf("Expected clusters %q, got %q", clusters, env.Ramen.Clusters)
+	}
+}
+
+func TestConfigEnvfileLoadHubless(t *testing.T) {
+	path := filepath.Join("testdata", "hubless.yaml")
+	env, err := envfile.Load(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if env.Name != "hubless" {
+		t.Errorf("Expected name %q, got %q", "e2e", env.Name)
+	}
+
+	if env.Ramen.Hub != "" {
+		t.Errorf("Expected hub %q, got %q", "hub", env.Ramen.Hub)
+	}
+	clusters := []string{"dr1", "dr2"}
+	if !reflect.DeepEqual(env.Ramen.Clusters, clusters) {
+		t.Errorf("Expected clusters %q, got %q", clusters, env.Ramen.Clusters)
+	}
+}
--- go.mod
+++ go.mod
@@ -8,9 +8,14 @@ go 1.19
 require (
 	github.com/spf13/cobra v1.7.0
 	k8s.io/client-go v0.28.0
+	sigs.k8s.io/yaml v1.3.0
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
